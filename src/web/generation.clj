a2909968c05c5e417aa94b5874cdaa7b23f9ce12
Rupa Shankar <rupsshankar@gmail.com>
2013-12-04 16:21:33 -0500

    Changed update-state and get-state-name to reflect the new format of having a s at the beginning of a state definition

--- src/web/generation.clj
+++ src/web/generation.clj
@@ -6,8 +6,8 @@
 
 (def test-input
   "S0 START A
-0 ----> S1
-1 ----> S0
+0 -> S1
+1 -> S0
 S1
 0 -> S4
 1 -> s5
@@ -18,17 +18,31 @@ END
 
 (defn is-state-line?
   [line]
-  (if (re-find (re-matcher #"->" line))
+  (if (re-find (re-matcher #"\s->\s" line))
     false
     true))
+
+(defn is-transition-line?
+  [line]
+  (if (re-find (re-matcher #"\s->\s" line))
+    true
+    false))
+
+(defn is-state-tag?
+  [tag]
+  (= (str/lower-case tag) "s"))
 
 (def test-dfa {'S0 {:start true :result :accept :transitions {0 'S0 1 'S1}}
                'S1 {:result :reject :transitions {0 'S0 1 'S1}}})
 
 (defn get-state-name
-  "e.g S1 or S0 start A or S1 A"
+  "e.g s S1 or s S0 start A or s S1 A
+  returns nil if the line is in the wrong format, the state name if not"
   [state-line]
-  (first (str/split state-line #"[\s]")))
+  (let [[state-tag state-name & options] (str/split state-line #"\s+")]
+    (if (is-state-tag? state-tag)
+      state-name
+      nil)))
 
 (defn result-info
   [options]
@@ -47,7 +61,7 @@ END
   [state-line]
   (println "\n...Updating state....")
   (println "curr line = " state-line)
-  (let [options (rest (str/split state-line #"[\s]"))
+  (let [[state-tag state-name & options] (str/split state-line #"\s+")
         result (merge (get-empty-state) (result-info options) (start-info options))]
     (println "UPDATED STATE = " result)
     result))
@@ -72,21 +86,25 @@ END
   (= "end" (str/lower-case (str/trim line))))
 
 (defn update-args
-  "returns updated [states curr-state-name curr-state"
+  "returns updated [states curr-state-name curr-state]"
   [states curr-state-name curr-state line]
-  (let [line (str/trim line)]
-    (if (is-state-line? line)
-      (do
-        (let [new-state-name (get-state-name line)
-              updated-state (update-state line)]
-          (if (or (not (nil? curr-state-name)) (is-end-line? line))
-            [(add-new-state states curr-state-name curr-state) new-state-name updated-state]
-            (do
-              (println "\n...Update-args...\n")
-              (println "updated state = " updated-state)
-              (println "to return = " [states new-state-name updated-state])
-              [states new-state-name updated-state]))))
-      [states curr-state-name (add-transition curr-state line)])))
+  (let [line (str/trim line)
+        new-state-name (get-state-name line)]
+    (cond
+     new-state-name
+     (let [updated-state (update-state line)]
+       (if (or (not (nil? curr-state-name)) (is-end-line? line))
+         [(add-new-state states curr-state-name curr-state)
+          new-state-name
+          updated-state]
+         (do
+           (println "\n...Update-args...\n")
+           (println "updated state = " updated-state)
+           (println "to return = " [states new-state-name updated-state])
+           [states new-state-name updated-state])))
+     (is-transition-line? line)
+     [states curr-state-name (add-transition curr-state line)]
+     :else nil)))
 
 (defn parse-dfa
   [state-transitions]
@@ -107,9 +125,12 @@ END
           (let [line (str/trim (nth lines i))
                 [new-states new-curr-state-name new-curr-state]
                 (update-args states curr-state-name curr-state line)]
-            (println "*****")
-            (println "new curr state = " new-curr-state)
-           (recur (inc i) new-states new-curr-state-name new-curr-state)))))))
+            (if (and (nil? new-states) (nil? new-curr-state-name) (nil? new-curr-state))
+              nil
+              (do
+                (println "*****")
+                (println "new curr state = " new-curr-state)
+                (recur (inc i) new-states new-curr-state-name new-curr-state)))))))))
 
 (defn test
   []
